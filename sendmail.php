3594d80708cad9b5177b5f4483eab9b885619760
Wen1750686723 <wen1750686723@qq.com>
2015-08-27 10:27:04 +0800

    goag

--- sendmail.php
+++ sendmail.php
@@ -8,19 +8,23 @@
 // 	echo "shibai";
 // }
 require_once ('email.class.php');
+function ss($title="",$body="",$touser="wen1750686723@qq.com",$type="HTML"){
+
+
 //##########################################
 $smtpserver = "smtp.163.com";//SMTP服务器
 $smtpserverport =25;//SMTP服务器端口
 $smtpusermail = "18730261183@163.com";//SMTP服务器的用户邮箱
-$smtpemailto = "wen1750686723@qq.com";//发送给谁
+$smtpemailto = $touser;//发送给谁
 $smtpuser = "18730261183";//SMTP服务器的用户帐号
-$smtppass = "wenbo744037";//SMTP服务器的用户密码
-$mailsubject = "PHP100测试邮件系统";//邮件主题
-$mailbody = "<h1> 这是一个测试程序 PHP100.com </h1>";//邮件内容
-$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
+$smtppass = "mima";//SMTP服务器的用户密码
+$mailsubject = $title;//邮件主题
+$mailbody = $body;//邮件内容
+$mailtype = $type;//邮件格式（HTML/TXT）,TXT为文本邮件
 ##########################################
 $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
-$smtp->debug = true;//是否显示发送的调试信息
+$smtp->debug = false;//是否显示发送的调试信息
 $smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
-
+}
+ss("测试信息","<h1><a href=\"baidu.com\">我用原生的代码编的</a></h1>","18889871413@qq.com");
 ?>
